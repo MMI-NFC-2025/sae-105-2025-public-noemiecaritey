c556712f63bffdb97f9b59e858ef4513ae534d90
noemiecaritey <ncaritey@edu.univ-fcomte.fr>
2025-12-10 16:26:00 +0100

    menu fonctionnel

--- src/js/script.js
+++ src/js/script.js
@@ -1,51 +1,55 @@
 // Carousel Programme
 document.addEventListener('DOMContentLoaded', function() {
   const carouselTrack = document.querySelector('.carousel__track');
-  const carouselLink = carouselTrack.querySelector('.carousel__link');
-  const carouselImage = carouselTrack.querySelector('img');
-  const programmeTitle = document.querySelector('.section__title');
-  const btnPrev = document.querySelector('.carousel__btn--prev');
-  const btnNext = document.querySelector('.carousel__btn--next');
+  
+  // Carousel uniquement si les éléments existent
+  if (carouselTrack) {
+    const carouselLink = carouselTrack.querySelector('.carousel__link');
+    const carouselImage = carouselTrack.querySelector('img');
+    const programmeTitle = document.querySelector('.section__title');
+    const btnPrev = document.querySelector('.carousel__btn--prev');
+    const btnNext = document.querySelector('.carousel__btn--next');
 
-  // Les slides du carousel
-  const slides = [
-    {
-      image: 'assets/img/image2.avif',
-      title: 'Programme',
-      link: 'assets/programme.html'
-    },
-    {
-      image: 'assets/img/image12.avif',
-      title: 'Historique',
-      link: 'assets/historique.html'
-    },
-    {
-      image: 'assets/img/image6.avif',
-      title: 'Scènes',
-      link: 'assets/scenes.html'
+    // Les slides du carousel
+    const slides = [
+      {
+        image: 'assets/img/image2.avif',
+        title: 'Programme',
+        link: 'assets/programme.html'
+      },
+      {
+        image: 'assets/img/image12.avif',
+        title: 'Historique',
+        link: 'assets/historique.html'
+      },
+      {
+        image: 'assets/img/image6.avif',
+        title: 'Scènes',
+        link: 'assets/scenes.html'
+      }
+    ];
+
+    let currentSlide = 0;
+
+    // Fonction pour mettre à jour le carousel
+    function updateCarousel() {
+      carouselImage.src = slides[currentSlide].image;
+      carouselLink.href = slides[currentSlide].link;
+      programmeTitle.textContent = slides[currentSlide].title;
     }
-  ];
 
-  let currentSlide = 0;
+    // Bouton suivant (flèche droite)
+    btnNext.addEventListener('click', function() {
+      currentSlide = (currentSlide + 1) % slides.length;
+      updateCarousel();
+    });
 
-  // Fonction pour mettre à jour le carousel
-  function updateCarousel() {
-    carouselImage.src = slides[currentSlide].image;
-    carouselLink.href = slides[currentSlide].link;
-    programmeTitle.textContent = slides[currentSlide].title;
+    // Bouton précédent (flèche gauche)
+    btnPrev.addEventListener('click', function() {
+      currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+      updateCarousel();
+    });
   }
-
-  // Bouton suivant (flèche droite)
-  btnNext.addEventListener('click', function() {
-    currentSlide = (currentSlide + 1) % slides.length;
-    updateCarousel();
-  });
-
-  // Bouton précédent (flèche gauche)
-  btnPrev.addEventListener('click', function() {
-    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-    updateCarousel();
-  });
 
   // Menu
   const menuBtn = document.querySelector('.menu-btn');
@@ -53,14 +57,18 @@ document.addEventListener('DOMContentLoaded', function() {
   const menuClose = document.querySelector('.menu__close');
 
   // Ouvrir le menu
-  menuBtn.addEventListener('click', function() {
-    menu.setAttribute('aria-hidden', 'false');
-    menuBtn.setAttribute('aria-expanded', 'true');
-  });
+  if (menuBtn) {
+    menuBtn.addEventListener('click', function() {
+      menu.setAttribute('aria-hidden', 'false');
+      menuBtn.setAttribute('aria-expanded', 'true');
+    });
+  }
 
   // Fermer le menu
-  menuClose.addEventListener('click', function() {
-    menu.setAttribute('aria-hidden', 'true');
-    menuBtn.setAttribute('aria-expanded', 'false');
-  });
+  if (menuClose) {
+    menuClose.addEventListener('click', function() {
+      menu.setAttribute('aria-hidden', 'true');
+      menuBtn.setAttribute('aria-expanded', 'false');
+    });
+  }
 });
